15452c441bfe3962d22e2b75a65a55aa0fda6353
Nithya Natarajan <nithyanatarajn@gmail.com>
2025-03-20 16:26:43 +0530

    Add a demo using browser-use with gemini model

--- sample/google_agent.py
+++ sample/google_agent.py
@@ -8,7 +8,7 @@ from pydantic import SecretStr
 load_dotenv()
 
 
-def get_agent_for(task: str, model='gemini-2.0-flash-exp'):
+def get_agent_for(task: str, model: str = 'gemini-2.0-flash-exp'):
     llm = ChatGoogleGenerativeAI(
         model=model, api_key=SecretStr(os.getenv('GEMINI_API_KEY'))
     )
